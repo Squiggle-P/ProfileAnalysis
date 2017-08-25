ce2af88ba9cb1f6e282cd73e95ed9dc9fda65154
Squiggle-P <palmer.price@gmail.com>
2017-08-25 16:13:59 -0500

    Actual Revision 0.0 for Transport Sample CSV for editing available for D/L

--- ProfileReader.py
+++ ProfileReader.py
@@ -1,2 +1,10 @@
-# Testing GIT
-# And again - where is the commit?
+""" Boilerplate Info
+ProfileReader.PY - Import profile data from CSV [R0], scrub it, export standardized format
+Created 08/25/17/PP - [R0]:
+
+
+Edited  MM/DD/YY/NN - [R1]:
+
+
+
+"""
